99df9ef2835494afd2d7d19491532ae29432d087
worldmozara <worldmozara@163.com>
2025-05-12 23:44:44 +0800

    chore: rename bare script mods folder to scripts

--- src/zeus-shim.py
+++ src/zeus-shim.py
@@ -4,17 +4,17 @@ import sys
 from zeusapi.internals.javaside import PythonRunner
 from zeusapi.internals.typedef import JepInterpreter
 
-sys.path.insert(0, "python/mods")
+sys.path.insert(0, "python/scripts")
 
 runners: list[PythonRunner] = []
 
-for mod in os.listdir("python/mods"):
+for mod in os.listdir("python/scripts"):
     if mod.startswith("_"):
         continue
     r = PythonRunner()
     @r.execute
-    def _import(interpreter: JepInterpreter) -> None:
-        interpreter.set("mod", mod)  # noqa: B023
+    def _import(interpreter: JepInterpreter, /, mod: str = mod) -> None:
+        interpreter.set("mod", mod)
         interpreter.exec("import importlib; importlib.import_module(mod)")
     _ = _import  # avoid TC complains about unused function
     runners.append(r)
@@ -22,11 +22,14 @@ for mod in os.listdir("python/mods"):
 pkmods = {mod[:-4] for mod in os.listdir("mods") if mod.lower().endswith(".pyz")}
 pkwmods = {mod[:-5] for mod in os.listdir("mods") if mod.lower().endswith(".pyzw")}
 
+if pkmods & pkwmods:
+    ...
+
 for m in pkmods:
     r = PythonRunner()
     @r.execute
-    def _import(interpreter: JepInterpreter) -> None:
-        interpreter.set("mod", m)  # noqa: B023
+    def _import(interpreter: JepInterpreter, /, mod: str = m) -> None:
+        interpreter.set("mod", mod)
         interpreter.exec("from zeusapi.internals import zimport; zimport(mod)")
     _ = _import  # avoid TC complains about unused function
     runners.append(r)
