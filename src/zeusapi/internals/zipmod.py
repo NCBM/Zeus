40e459740ee336d4c1463d06f19996d176a86e1d
worldmozara <worldmozara@163.com>
2025-05-12 23:47:55 +0800

    feat: allow to specify filename differs from modname

--- src/zeusapi/internals/zipmod.py
+++ src/zeusapi/internals/zipmod.py
@@ -1,14 +1,18 @@
 import importlib.util
 import sys
+from types import ModuleType
 import zipimport
 
 
-def zimport(modname: str):
-    imp = zipimport.zipimporter(f"mods/{modname}.pyz")
+def zimport(modname: str, filename: str | None = None) -> ModuleType:
+    filename = f"mods/{modname}.pyz" if filename is None else f"mods/{filename}"
+    imp = zipimport.zipimporter(filename)
     spec = imp.find_spec(modname)
     if spec is None:
-        raise ModuleNotFoundError(f"No mod module named {modname!r} in {modname+'.pyz'!r}")
+        raise ModuleNotFoundError(f"No mod module named {modname!r} in {filename!r}.")
     module = importlib.util.module_from_spec(spec)
     sys.modules[modname] = module
+    if spec.loader is None:
+        raise ImportError(f"Cannot load module {modname!r}.")
     spec.loader.exec_module(module)
     return module
